7a828c1873558cd8275151367c4d1d15cf8ada8c
juliusdc14 <juliusdc05@gmail.com>
2022-09-15 09:47:48 -0500

    setup dapper functions

---
+++ introSQL/DapperDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace introSQL
+{
+    public class DapperDepartmentRepository
+    {
+        public DapperDepartmentRepository()
+        {
+        }
+    }
+}
+
---
+++ introSQL/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace introSQL
+{
+    public class Department
+    {
+        public Department()
+        {
+        }
+    }
+}
+
